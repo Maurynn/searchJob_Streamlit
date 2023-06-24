4a401445b9ac8fc8cd64ed11fb36540440a373e9
Mauro Alves <mauro.mn@hotmail.com>
2023-06-24 17:23:00 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -75,5 +75,8 @@ if st.button('Buscar'):
         st.markdown(f"<h4 style='text-align: left; color: whyte;'>Vagas de Dev {description} por cidade!</h4>", unsafe_allow_html=True)
         st.pyplot(fig)
 
+for _ in range(20):
+    st.markdown("<br>", unsafe_allow_html=True)
+
 st.markdown("___")
 st.markdown("Em Desenvolvimento por Mauro Alves")     
