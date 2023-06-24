6d088faa31330eca25f3863895008c1c0e7e5ef7
Mauro Alves <mauro.mn@hotmail.com>
2023-06-24 16:36:37 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ if st.button('Buscar'):
         fig, ax = plt.subplots()
         ax.bar(location_counts.keys(), location_counts.values())
         plt.xticks(rotation=90)
-        st.header(f"<h2 style='text-align: center; color: cyan;'>Vagas de Dev {description} por cidade!</h2>", unsafe_allow_html=True)
+        st.markdown(f"<h3 style='text-align: center; color: cyan;'>Vagas de Dev {description} por cidade!</h3>", unsafe_allow_html=True)
 
         st.pyplot(fig)
 
