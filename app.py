61552101a9848b9cb877d0f305e4ec25a29c8a9f
Mauro Alves <mauro.mn@hotmail.com>
2023-06-24 20:43:42 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -10,22 +10,17 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 # Configuração da API do Adzuna
-API_URL = "https://api.adzuna.com/v1/api/jobs/br/search/{page}" # substitua {page} pelo número da página
+API_URL = "https://api.adzuna.com/v1/api/jobs/br/search/1" # substitua {country} pelo código do país
 API_KEY = "f2471fc865692b0445fa6efd1f65c765" # substitua pelo sua chave de API 
 APP_ID = "d0210377" # substitua pelo seu App ID
-
-# Configuração inicial
-page = 1
-description = ''
-location = ''
-
 # Título do aplicativo
 st.markdown("<h1 style='text-align: center; color: cyan;'>Hey, Dev! 👨🏻‍💻 PROCURE SUA VAGA DE- EMPREGO AQUI! !</h1>", unsafe_allow_html=True)
 
+
 # Criar campos para os filtros de busca
 search_description, search_location = st.columns(2)
-description = search_description.text_input('Descrição da vaga (por exemplo, Python, JavaScript, etc.)', description)
-location = search_location.text_input('Localização', location)
+description = search_description.text_input('Descrição da vaga (por exemplo, Python, JavaScript, etc.)')
+location = search_location.text_input('Localização')
 
 # Adicionar botão de buscar
 if st.button('Buscar'):
@@ -33,13 +28,13 @@ if st.button('Buscar'):
     params = {
         'app_id': APP_ID,
         'app_key': API_KEY,
-        'results_per_page': 10,
+        'results_per_page': 15,
         'what': description,
         'where': location
     }
 
     # Fazer a requisição para a API do Adzuna com os parâmetros de busca
-    response = requests.get(API_URL.format(page=page), params=params)
+    response = requests.get(API_URL, params=params)
 
     # Verificar se a requisição foi bem-sucedida
     if response.status_code == 200:
@@ -80,14 +75,6 @@ if st.button('Buscar'):
         plt.xticks(rotation=90)
         st.markdown(f"<h4 style='text-align: left; color: whyte;'>Vagas de Dev {description} por cidade!</h4>", unsafe_allow_html=True)
         st.pyplot(fig)
-
-        # Botões para a navegação de página
-        previous_button, next_button = st.columns(2)
-        if page > 1:
-            if previous_button.button('Anterior'):
-                page -= 1
-        if next_button.button('Próximo'):
-            page += 1
 
 st.markdown("___")
 st.markdown("in development by Mauro Alves:")
